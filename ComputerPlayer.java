e1e70296659d2f2b9afe2be74e6281cae66f88c1
tungqd <tungqd90@yahoo.com>
2012-03-08 17:22:08 -0800

    Modified version using encapsulation

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -1,31 +1,26 @@
 package RPS;
-
 /**
- * A class that represents one player of the RPS game, this implements the abstract Player class.
+ * A class that represents computer player of the RPS game, this implements the abstract Player class.
  * @author KAI
  */
-import java.util.Random;
-
 
 public class ComputerPlayer extends Player
-{Random Random=new Random();
-  
+{
+    ThrowCalculator throwcalc=new RandomThrow();
+
   public ComputerPlayer()
     {stats=new Stats();
-     
+
     }
-
-
+  
     /**
-     * This returns the throw for this player, or a -1 if the throw is read from
-     * another location or an error occurs.
-     * @return the throw for this player or -1.
+     * This returns the throw for computer player
+     * @return the throw for computer
      */
     @Override
     int getThrows()
     {
-        int computerChoice=Random.nextInt(3);
-        return computerChoice;
+        return throwcalc.calculatethrow();
     }
 
 }
