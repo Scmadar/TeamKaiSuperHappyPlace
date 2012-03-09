897d3076b8da44b7e63529dfff1cb4f007c5f452
tungqd <tungqd90@yahoo.com>
2012-03-08 17:25:23 -0800

    Modified version using encapsulation

--- RPSFrontEnd.java
+++ RPSFrontEnd.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package RPS;
 import java.util.*;
 
@@ -15,6 +11,7 @@ public class RPSFrontEnd
   static Player player2=new ComputerPlayer();
   static Scanner in=new Scanner(System.in);
   static Match current=null;
+  static Parser parser=new ParseCommandLine();
 
  /**
   * @param args the command line arguments
@@ -36,10 +33,11 @@ public class RPSFrontEnd
     int command=-1;
     while(command==-1)
       {printString("\n\t MAIN MENU\n");
-       printString("Enter 1 to start a match\n");
-       printString("Enter 2 to display help\n");
-       printString("Enter 3 to display score\n");
-       printString("Enter 4 to quit the game\n");
+       printString("Enter 1 or 'match' to start a match\n");
+       printString("Enter 2 or 'help' display help\n");
+       printString("Enter 3 or 'score' to display score\n");
+       printString("Enter 4 or 'quit' to quit the game\n");
+       printString("Enter your choice: ");
        command=parceMenuCommand(getCommand());
       }
     }
@@ -49,19 +47,34 @@ public class RPSFrontEnd
   * Begins a match.
   */
  private static void startMatch()
-   {int winner;
+   {
+    int winner;
     current=new Match(throwCount);
     while(!current.matchIsOver())
       {winner=0;
        int p1Throw=player1.getThrows();
        while(p1Throw==-1)
          {printString("Enter your throw: ");
-          p1Throw=parceCommand(getCommand());
+          p1Throw=parser.parceCommand(getCommand());
           if(p1Throw==-1)
             {printString("Invalid command. Please enter rock, paper,"
                                 +" or scissors.\n");}
           if(p1Throw==3)
-            {p1Throw=-1;}
+          {
+                  displayHelp();
+                  p1Throw = -1;
+          }
+          if(p1Throw==4)
+          {
+              displayScore();
+              p1Throw = -1;
+          }
+          if(p1Throw==5)
+          {
+              printString("Goodbye!\n");
+              System.exit(0);
+          }
+              
          }
        printString("You chose ");
        switch (p1Throw)
@@ -73,18 +86,31 @@ public class RPSFrontEnd
               break;
           default: printString("This should be impossibble.\n");
          }
-           
-       
+
+
        int p2Throw=player2.getThrows();
-       while(p2Throw==-1)
+       
+       // This code is for 2 players
+       /*while(p2Throw==-1)
          {printString("Enter your throw: ");
-          p2Throw=parceCommand(getCommand());
+          p2Throw=parser.parceCommand(getCommand());
           if(p2Throw==-1)
             {printString("Invalid command. Please enter rock, paper,"
                                 +" or scissors.\n");}
           if(p2Throw==3)
-            {p2Throw=-1;}
-         }
+          {
+                  displayHelp();
+                  p2Throw = -1;
+          }
+          if(p2Throw==4)
+          {
+              displayScore();
+              p2Throw = -1;
+          }
+          if(p2Throw==5)
+              System.exit(0);
+         }*/
+       
        printString("The computer chose ");
        switch (p2Throw)
          {case 0: printString("Rock\n");
@@ -95,105 +121,73 @@ public class RPSFrontEnd
               break;
           default: printString("This should be impossibble.\n");
          }
-       
+
        winner=current.checkRound(p1Throw, p2Throw);
        if(winner==0)//tie
          {printString("The round ended in a tie!\n");
           player1.incrementTies();
-          player2.incrementTies();
+         // player2.incrementTies();
          }
        else if(winner==-1)//p1 wins
          {printString("You won the round!\n");
           player1.incrementWins();
-          player2.incrementLosses();
+          //player2.incrementLosses();
        }
        else if(winner==1)
          {printString("The computer won the round!\n");
           player1.incrementLosses();
-          player2.incrementWins();
+          //player2.incrementWins();
          }
        else
          {printString("Something went horribly wrong.");}
-       
+
       }
     displayScore();
     current=null;
 
    }
- 
+
 /**
  *
  * @return the String returned by the scanner. Later will be button presses.
  */
  private static String getCommand()
-   {return in.nextLine();
+   {
+     return in.nextLine();
    }
 
- 
- 
+
+/**
+ * This handles the initial menu command
+ * @param parceMe the input from the player
+ * @return a -1 to continue the loop if nothing is changed
+ */
  private static int parceMenuCommand(String parceMe)
-   {if(parceMe==null)
+   {
+     if(parceMe==null)
       {return -1;}
-    if(parceMe.equals("1"))
+    if(parceMe.equals("1")|| parceMe.equalsIgnoreCase("match"))
       {startMatch();
        return -1;
       }
-    if(parceMe.equals("2"))
+    if(parceMe.equals("2")|| parceMe.equalsIgnoreCase("help"))
       {displayHelp();
        return -1;
       }
-    if(parceMe.equals("3"))
+    if(parceMe.equals("3")|| parceMe.equalsIgnoreCase("score"))
       {displayScore();
        return -1;
       }
-    if(parceMe.equals("4"))
+    if(parceMe.equals("4")|| parceMe.equalsIgnoreCase("quit"))
       {printString("Thanks for playing!\n\n");
        System.exit(0);
       }
+
     printString("Command not understood. Please try again.\n");
-    
     return -1;
    }
- 
- 
- 
- /**
-  *
-  * @param parceMe - a non-null String object
-  * @return integer value for the choice selected. 0=rock, 1=paper,
-  *       2=scissors. 3 is other command successfully executed.
-  *       -1 is an error of any sort.
-  */
- private static int parceCommand(String parceMe)
-   {if(parceMe==null)
-      {return -1;
-      }
-    if(parceMe.equalsIgnoreCase("rock"))
-      {return 0;
-      }
-    else if(parceMe.equalsIgnoreCase("paper"))
-      {return 1;
-      }
-    else if(parceMe.equalsIgnoreCase("scissors"))
-      {return 2;
-      }
-    else if(parceMe.equalsIgnoreCase("HELP")||
-          parceMe.equalsIgnoreCase("H"))
-      {displayHelp();
-       return 3;
-      }
-    else if(parceMe.equalsIgnoreCase("score"))
-      {displayScore();
-       return 3;
-      }
-    else if(parceMe.equalsIgnoreCase("quit"))
-      {printString("Goodbye!\n\n");
-       System.exit(0);
-      }
-    return -1; //if it got past return an error
-   }
- 
- 
+
+
 /**
  *
  * @param printMe the string to be printed.
@@ -234,18 +228,21 @@ public class RPSFrontEnd
        printString("Player 2 wins: "+temp[1]+"\n");
        printString("Ties: "+temp[2]+"\n\n");
        printString("Current match progress: "+current.getThrowCount()
-                +" of "+throwCount+" throws completed.\n\n");
+                +" of "+throwCount+" throws left.\n\n");
       }
     Stats temp=player1.getStats();
-    printString("===Player 1 stats===\n");
+    printString("===Player stats===\n");
     printString("Wins: "+temp.getWins()+"\n");
     printString("Losses: "+temp.getLosses()+"\n");
     printString("Ties: "+temp.getTies()+"\n\n");
-    
+
+    /*
     temp=player2.getStats();
     printString("===Player 2 stats===\n");
     printString("Wins: "+temp.getWins()+"\n");
     printString("Losses: "+temp.getLosses()+"\n");
     printString("Ties: "+temp.getTies()+"\n\n");
+     *
+     */
    }
 }
