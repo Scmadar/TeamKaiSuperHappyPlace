41291d74fd4eab1dced83a2631aff8d7aa507161
Tung Dang <tungqd90@yahoo.com>
2012-04-19 13:50:58 -0700

    Add DisableField

--- NewJFrame.java
+++ NewJFrame.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package RPS;
 
 import java.util.Scanner;
@@ -9,7 +5,7 @@ import javax.swing.*;
 
 /**
  *
- * @author Think
+ * @author KAI
  */
 public class NewJFrame extends javax.swing.JFrame {
 
@@ -384,8 +380,6 @@ public class NewJFrame extends javax.swing.JFrame {
            throwcountTextField.setText(new Integer(defaultThrowCount).toString());
           }
           
-          
-        
         
     }
     public void disableFields() {
